2b1165ed870fffa3721f6237b3d4cacf6f37bc8d
steinhakasevs <stein.hakase.vs@gmail.com>
2020-09-06 00:12:24 -0600

    replaced static values for variables

--- components/recipieCard.js
+++ components/recipieCard.js
@@ -7,7 +7,7 @@ const template = ({
   userName = ''
 } = {}) => {
   return /* html */`
-  <div class="card" style="width: 22rem; box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);">
+  <div class="card" style="box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);">
     <img src="${image}" class="card-img-top" />
     <div class="card-body px-2 py-3">
       <div class="row">
@@ -17,18 +17,18 @@ const template = ({
         </div>
         <div class="col-4 p-0">
           <div class="row d-flex justify-content-center align-items-center">
-            <i class="fas fa-star mr-2" style="color: #FCD127; font-size: 24px"></i>
+            <i class="fas fa-star mr-2" style="color: var(--yellow); font-size: 24px"></i>
             <p class="card-text font-weight-light" style="font-size: 15px">${stars}</p>
           </div>
         </div>
       </div>
       <div class="row mt-4">
-        <div class="col-3">
+        <div class="col-4">
           <img src="${userPicture}" class="rounded-circle" style="max-width: 59px" />
         </div>
-        <div class="col-9 pl-0 d-flex-row m-auto">
-          <p class="card-title font-weight-light mb-0" style="font-size: 15px; color: #F42434">Submited by:</p>
-          <p class="card-title font-weight-bold mb-0" style="font-size: 15px; color: #F42434">${userName}</p>
+        <div class="col-8 pl-0 d-flex-row m-auto">
+          <p class="card-title font-weight-light mb-0" style="font-size: 15px; color: var(--red);">Submited by:</p>
+          <p class="card-title font-weight-bold mb-0" style="font-size: 15px; color: var(--red);">${userName}</p>
         </div>
       </div>
     </div>
@@ -47,8 +47,6 @@ class RecipieCard extends HTMLElement {
       acc[prop] = this.getAttribute(prop) ? this.getAttribute(prop) : undefined
       return acc
     }, {})
-
-    console.log(templateProps)
 
     // Attach stuff to shadow DOM
     this.innerHTML = template(templateProps)
