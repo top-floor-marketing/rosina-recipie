ea5eda2a78154132150afabc2ba47ac773618de6
steinhakasevs <stein.hakase.vs@gmail.com>
2020-09-07 00:16:54 -0600

    fixed some recipie cards responsive issues

--- components/recipieCard.js
+++ components/recipieCard.js
@@ -1,18 +1,20 @@
-const template = ({
+const templateRecipieCard = ({
   title = '',
-  image = './assets/recipie-placeholder.png',
+  picture = './assets/img/recipie-placeholder.png',
   subtitle = '',
-  userPicture = './assets/profile-placeholder.jpg',
+  userPicture = './assets/img/profile-placeholder.jpg',
   stars = 0,
   userName = ''
 } = {}) => {
   return /* html */`
-  <div class="card" style="box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);">
-    <img src="${image}" class="card-img-top" />
-    <div class="card-body px-2 py-3">
-      <div class="row">
+  <div class="card" style="box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23); min-height: 28rem;">
+    <img src="${picture}" class="card-img-top" />
+    <div class="card-body px-2 py-3 d-flex flex-column">
+      <div class="row mb-3">
         <div class="col-8">
-          <p class="card-title font-weight-bold mb-0" style="font-size: 20px">${title}</p>
+          <p class="card-title font-weight-bold mb-0" title="${title}" style="font-size: 20px">
+            ${title.length > 22 ? `${title.slice(0, 21)}...` : title}
+          </p>
           <p class="card-text font-weight-light" style="font-size: 15px">${subtitle}</p>
         </div>
         <div class="col-4 p-0">
@@ -22,13 +24,15 @@ const template = ({
           </div>
         </div>
       </div>
-      <div class="row mt-4">
-        <div class="col-4">
+      <div class="row mt-auto mw-100">
+        <div class="d-flex align-items-center col-3 col-sm-4">
           <img src="${userPicture}" class="rounded-circle" style="max-width: 59px" />
         </div>
-        <div class="col-8 pl-0 d-flex-row m-auto">
+        <div class="d-flex-row col-9 col-sm-8 pl-0 m-auto">
           <p class="card-title font-weight-light mb-0" style="font-size: 15px; color: var(--red);">Submited by:</p>
-          <p class="card-title font-weight-bold mb-0" style="font-size: 15px; color: var(--red);">${userName}</p>
+          <p class="card-title font-weight-bold mb-0" style="font-size: 15px; color: var(--red);">
+            ${userName.length > 22 ? `${userName.slice(0, 21)}...` : userName}
+          </p>
         </div>
       </div>
     </div>
@@ -48,8 +52,8 @@ class RecipieCard extends HTMLElement {
       return acc
     }, {})
 
-    // Attach stuff to shadow DOM
-    this.innerHTML = template(templateProps)
+    // Attach stuff to DOM
+    this.innerHTML = templateRecipieCard(templateProps)
   }
 }
 
