50b58c99ee63ce4c34096f533afdd14b1e21a12c
ggotfm <ggo@topfloormarketing.net>
2020-09-08 00:16:29 -0600

    Step one CSS Finished for step buttons

--- components/steps/stepOne.js
+++ components/steps/stepOne.js
@@ -1,15 +1,25 @@
 const stepOneTemplate = ({
   testProp = 'test'
 } = {}) => {
-  return `
+  return /* html */ `
     <div class='row'>
-      <div class='col-md-6'>
+      <div class='col-md-6 text-right responsive-align-center'>
         <label class="switch">
           <input type="checkbox">
-          <span class="slider round"></span>
+          <span class="slider round d-flex align-items-center">
+            <img src="../../../assets/img/Fork.svg" class="img-fluid toggler" alt="">
+            <p class='in-text my-0 ml-2 font-weight-bold'>Pasta</p>
+          </span>
         </label>
       </div>
-      <div class='col-md-6'>
+      <div class='col-md-6 text-left responsive-align-center'>
+        <label class="switch">
+          <input type="checkbox">
+          <span class="slider round d-flex align-items-center">
+            <img src="../../../assets/img/Meatballs.svg" class="img-fluid toggler" alt="">
+            <p class='in-text my-0 ml-2 font-weight-bold'>Meat Balls</p>
+          </span>
+        </label>
       </div>
     </div>
     `
@@ -19,7 +29,6 @@ class stepOne extends HTMLElement {
   // Fires when an instance of the element is created or updated
   constructor () {
     super()
-
     const props = ['testProp']
     // Get all the props
     const templateProps = props.reduce((acc, prop) => {
