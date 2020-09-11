ab6d5320563789777278234fc517bd3c4644d901
ggotfm <ggo@topfloormarketing.net>
2020-09-10 21:16:04 -0600

    Finished step 2

---
+++ components/floatingWidget.js
@@ -0,0 +1,44 @@
+const widgetTemplate = ({
+  testProp = 'test'
+} = {}) => {
+  return /* html */`
+        <div class='animate__animated animate__fadeInUp widget-servings'>
+            <img src="./assets/img/plates.png" class="plate-serving img-fluid animate__animated" alt="">
+            <div class='d-flex align-items-center flex-column p-3 pl-5 shadow bg-white position-absolute rotating-widget'>
+                <h5 class='font-weight-bold'>Servings</h5>
+                <more-less servings='true' />
+            </div>
+        </div>
+        `
+}
+
+class floatingWidget extends HTMLElement {
+  // Fires when an instance of the element is created or updated
+  constructor () {
+    super()
+
+    this.state = {
+      servings: 1
+    }
+    this.innerHTML = widgetTemplate()
+    this.addEventListener('more-less', (event) => {
+      const { currAmount } = event.detail
+      this.state.servings = currAmount
+      recipeData.servings = this.state.servings
+      console.log(recipeData)
+    })
+  }
+}
+
+$(function () {
+  setInterval(function () {
+    var animationName = 'animate__tada'
+    var animationend = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend'
+    $('.plate-serving').addClass(animationName).one(animationend, function () {
+      $(this).removeClass(animationName)
+    })
+  }, 5000)
+})
+
+// Registers custom element
+window.customElements.define('floating-widget', floatingWidget)
--- components/moreLessCounter.js
+++ components/moreLessCounter.js
@@ -1,22 +1,42 @@
 
-const moreLessTemplate = () => {
-  return /* html */ `
-        <style>
-          @import "../assets/css/styles.css";
-          @import url("https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css");
-        </style>
-        <div class='more-less-container d-flex'>
-            <div class='more-less-plus text-right p-0'>
-                <button class='rounded-more-less-left'>-</button>
-            </div>
-            <div class='more-less-number p-0 width-60px'>
-                <input type='number' value='0.25' class='w-100 text-center more-less-number-input'>
-            </div>
-            <div class='more-less-minus text-left p-0'>
-                <button class='rounded-more-less-right'>+</button>
-            </div>
+const moreLessTemplate = ({ servings }) => {
+  if (servings === 'true') {
+    return /* html */ `
+    <style>
+      @import "../assets/css/styles.css";
+      @import url("https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css");
+    </style>
+    <div class='more-less-container d-flex'>
+        <div class='more-less-plus text-right p-0'>
+            <button class='rounded-more-less-left'>-</button>
         </div>
-        `
+        <div class='more-less-number p-0 width-60px'>
+            <input type='number' value='1' class='w-100 text-center more-less-number-input'>
+        </div>
+        <div class='more-less-minus text-left p-0'>
+            <button class='rounded-more-less-right'>+</button>
+        </div>
+    </div>
+    `
+  } else {
+    return /* html */ `
+          <style>
+            @import "../assets/css/styles.css";
+            @import url("https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css");
+          </style>
+          <div class='more-less-container d-flex'>
+              <div class='more-less-plus text-right p-0'>
+                  <button class='rounded-more-less-left'>-</button>
+              </div>
+              <div class='more-less-number p-0 width-60px'>
+                  <input type='number' value='0.25' class='w-100 text-center more-less-number-input'>
+              </div>
+              <div class='more-less-minus text-left p-0'>
+                  <button class='rounded-more-less-right'>+</button>
+              </div>
+          </div>
+          `
+  }
 }
 
 class moreLessInput extends HTMLElement {
@@ -24,8 +44,14 @@ class moreLessInput extends HTMLElement {
   constructor () {
     super()
 
+    const props = ['servings']
+    // Get all the props
+    this.templateProps = props.reduce((acc, prop) => {
+      acc[prop] = this.getAttribute(prop) ? this.getAttribute(prop) : undefined
+      return acc
+    }, {})
     const shadow = this.attachShadow({ mode: 'open' })
-    shadow.innerHTML = moreLessTemplate()
+    shadow.innerHTML = moreLessTemplate(this.templateProps)
   }
 
   checkAmount () {
@@ -42,13 +68,25 @@ class moreLessInput extends HTMLElement {
 
   connectedCallback () {
     this.shadowRoot.querySelector('.rounded-more-less-right').addEventListener('click', () => {
-      this.shadowRoot.querySelector('.more-less-number-input').value = parseFloat(this.shadowRoot.querySelector('.more-less-number-input').value) + 0.25
-      this.checkAmount()
+      if (this.templateProps.servings !== 'true') {
+        this.shadowRoot.querySelector('.more-less-number-input').value = parseFloat(this.shadowRoot.querySelector('.more-less-number-input').value) + 0.25
+        this.checkAmount()
+      } else {
+        this.shadowRoot.querySelector('.more-less-number-input').value = parseFloat(this.shadowRoot.querySelector('.more-less-number-input').value) + 1
+        this.checkAmount()
+      }
     })
     this.shadowRoot.querySelector('.rounded-more-less-left').addEventListener('click', () => {
-      if (this.shadowRoot.querySelector('.more-less-number-input').value > 0) {
-        this.shadowRoot.querySelector('.more-less-number-input').value = parseFloat(this.shadowRoot.querySelector('.more-less-number-input').value) - 0.25
-        this.checkAmount()
+      if (this.templateProps.servings !== 'true') {
+        if (this.shadowRoot.querySelector('.more-less-number-input').value > 0) {
+          this.shadowRoot.querySelector('.more-less-number-input').value = parseFloat(this.shadowRoot.querySelector('.more-less-number-input').value) - 0.25
+          this.checkAmount()
+        }
+      } else {
+        if (this.shadowRoot.querySelector('.more-less-number-input').value > 0) {
+          this.shadowRoot.querySelector('.more-less-number-input').value = parseFloat(this.shadowRoot.querySelector('.more-less-number-input').value) - 1
+          this.checkAmount()
+        }
       }
     })
   }
